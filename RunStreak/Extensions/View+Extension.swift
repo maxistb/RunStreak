b8e88ae449b32a71a7aaa1039f0d6bfedd4a61c0
Maximillian Stabe <maxjoesta@gmail.com>
2025-10-26 09:47:32 +0100

    added main UI layout

---
+++ RunStreak/Extensions/View+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  View+Extension.swift
+//  RunStreak
+//
+//  Created by Maximillian Stabe on 25.10.25.
+//
+
+import Foundation
---
+++ RunStreak/UIComponents/Card/DonationCards.swift
@@ -0,0 +1,8 @@
+//
+//  DonationCard.swift
+//  RunStreak
+//
+//  Created by Maximillian Stabe on 26.10.25.
+//
+
+import Foundation
